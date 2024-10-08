41160ec720683a91437eaaeaaa9b9a0745c518c8
Edgardo Ruotolo <edgardoruotolo@gmail.com>
2024-10-08 08:46:55 -0300

    Commit

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export function Header() {
     const bannerHome = '/banner-home.jpg';
@@ -14,7 +14,7 @@ export function Header() {
                 fill={true}
                 priority={true}
                 quality={100} // Maintain image quality
-                className="object-cover object-top md:block hidden"
+                className="hidden object-cover object-center md:block"
             />
             <Image
                 src={bannerHomeMobile}
@@ -22,18 +22,35 @@ export function Header() {
                 fill={true}
                 priority={true}
                 quality={100} // Maintain image quality
-                className="object-cover object-top block md:hidden"
+                className="block object-cover object-top md:hidden"
             />
             <div className="flex flex-col">
-                <div className="flex justify-between h-[21vh]">
-                    <Image src={lemaImg} alt={'lema'} width={300} height={130} className="z-20 ml-[5%] md:w-[300px] w-[150px]"/>
-                    <Image src={votaImg} alt={'Vota text'} width={200} height={200} className="z-20 md:w-[200px] w-[150px]"/>
+                <div className="flex h-[21vh] justify-between">
+                    <Image
+                        src={lemaImg}
+                        alt={'lema'}
+                        width={300}
+                        height={130}
+                        className="z-20 ml-[5%] w-[150px] md:w-[300px]"
+                    />
+                    <Image
+                        src={votaImg}
+                        alt={'Vota text'}
+                        width={200}
+                        height={200}
+                        className="z-20 w-[150px] md:w-[200px]"
+                    />
                 </div>
-                <div className="flex items-end md:h-[79vh] h-[64vh]">
-                    <Image src={lemaImg1} alt={'Lema 01'} width={750} height={375} className="z-20 md:w-[750px] w-[350px]"/>
+                <div className="flex h-[64vh] items-end md:h-[79vh]">
+                    <Image
+                        src={lemaImg1}
+                        alt={'Lema 01'}
+                        width={750}
+                        height={375}
+                        className="z-20 w-[350px] md:w-[750px]"
+                    />
                 </div>
             </div>
-
         </section>
-    )
+    );
 }
